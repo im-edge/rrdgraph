5ed42ef9288c6ca2a6ec172879f329a2e06a6327
Thomas Gelf <thomas@gelf.net>
2022-08-05 18:52:23 +0200

    AssignmentTest: parse and render doc samples

--- tests/Data/AssignmentTest.php
+++ tests/Data/AssignmentTest.php
@@ -8,10 +8,13 @@ use gipfl\RrdGraph\DataType\IntegerType;
 use gipfl\RrdGraph\Rpn\Add;
 use gipfl\RrdGraph\Rpn\Multiply;
 use gipfl\RrdGraph\Rpn\RpnExpression;
+use gipfl\Tests\RrdGraph\TestHelpers;
 use PHPUnit\Framework\TestCase;
 
 class AssignmentTest extends TestCase
 {
+    use TestHelpers;
+
     public function testDefineAndRenderMultiplication()
     {
         $def = new Assignment('DEF', new VariableName('result'), new RpnExpression(new Multiply(), [5, 6]));
@@ -30,5 +33,29 @@ class AssignmentTest extends TestCase
         $def->getRpnExpression()->renameVariable('test', 'changed')->renameVariable('subValue', 'sub');
 
         $this->assertEquals('DEF:uptime=changed,sub,10,+,*', (string) $def);
+    }
+
+    public function testVariableDefinitionCanBeParsedAndRendered()
+    {
+        $def = 'VDEF:avg=mydata,AVERAGE';
+        $this->parseAndRender($def);
+    }
+
+    public function testDataCalculationDefinitionCanBeParsedAndRendered()
+    {
+        $def = 'CDEF:mydatabits=mydata,8,*';
+        $this->parseAndRender($def);
+    }
+
+    public function testDataDefinitionCanBeParsedAndRendered()
+    {
+        $def = 'DEF:ds0=router.rrd:ds0:AVERAGE';
+        $this->parseAndRender($def);
+    }
+
+    public function testComplexDataDefinitionCanBeParsedAndRendered()
+    {
+        $def = 'DEF:ds0weekly=router.rrd:ds0:AVERAGE:step=7200:start=11\:00:end=start+1h:daemon=collect1.example.com';
+        $this->parseAndRender($def);
     }
 }
