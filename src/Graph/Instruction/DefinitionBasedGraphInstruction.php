418a632bc540f6ae311bd98c7e991510754eb78e
Thomas Gelf <thomas@gelf.net>
2022-08-05 22:10:55 +0200

    DefinitionBasedGraphInstruction: optional flags
    Could be improved, but suffices for now

--- src/Graph/Instruction/DefinitionBasedGraphInstruction.php
+++ src/Graph/Instruction/DefinitionBasedGraphInstruction.php
@@ -4,7 +4,9 @@ namespace gipfl\RrdGraph\Graph\Instruction;
 
 use gipfl\RrdGraph\Color;
 use gipfl\RrdGraph\Data\VariableName;
+use gipfl\RrdGraph\DataType\BooleanType;
 use gipfl\RrdGraph\DataType\StringType;
+use InvalidArgumentException;
 
 abstract class DefinitionBasedGraphInstruction implements GraphInstructionInterface
 {
@@ -12,6 +14,8 @@ abstract class DefinitionBasedGraphInstruction implements GraphInstructionInterf
     protected VariableName $definition;
     protected ?Color $color = null;
     protected ?StringType $legend = null;
+
+    const OPTIONAL_PARAMETERS = [];
 
     final public function __construct(VariableName $definition, ?Color $color = null, ?StringType $legend = null)
     {
@@ -25,19 +29,34 @@ abstract class DefinitionBasedGraphInstruction implements GraphInstructionInterf
         $variable = array_shift($parameters);
         $parts = explode('#', $variable);
         $varName = new VariableName(array_shift($parts));
-        foreach ($parameters as &$parameter) {
-            if (is_string($parameter)) {
-                $parameter = new StringType($parameter);
-            }
-        }
-        if (empty($parts)) {
-            array_unshift($parameters, null);
+        if (! empty($parts)) {
+            $color = new Color(array_shift($parts));
         } else {
-            foreach (array_reverse($parts) as $color) {
-                array_unshift($parameters, new Color($color));
+            $color = null;
+        }
+        foreach ($parameters as $key => $parameter) {
+            if (isset(static::OPTIONAL_PARAMETERS[$parameter])) {
+                $class = static::OPTIONAL_PARAMETERS[$parameter];
+                $parameters[$key] = $class::parse($parameter);
+            } else {
+                $parameters[$key] = StringType::parse($parameter);
             }
         }
-        array_unshift($parameters, $varName);
-        return new static(...$parameters);
+        if (! empty($parameters) && $parameters[0] instanceof StringType) {
+            $legend = array_shift($parameters);
+        } else {
+            $legend = null;
+        }
+
+        $self =  new static($varName, $color, $legend);
+        foreach ($parameters as $boolean) {
+            if ($boolean instanceof BooleanType) {
+                $self->{$boolean->getLabel()} = $boolean;
+            } else {
+                throw new InvalidArgumentException("Got unknown optional parameter: $boolean");
+            }
+        }
+
+        return $self;
     }
 }
