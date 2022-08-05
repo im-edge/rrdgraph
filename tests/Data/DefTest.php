90167214771a47a15bbedb5098359a105cc62c1d
Thomas Gelf <thomas@gelf.net>
2022-08-05 19:26:39 +0200

    StringType: no need to use quotes for slashes

--- tests/Data/DefTest.php
+++ tests/Data/DefTest.php
@@ -10,11 +10,11 @@ class DefTest extends TestCase
     public function testParsesAndRendersDefString()
     {
         $in = "DEF:'def_average_iowait'='2d/07/2d07f300dbec4dc5a30caa61ae76ca0a.rrd':'iowait':AVERAGE";
-        $out = "DEF:def_average_iowait='2d/07/2d07f300dbec4dc5a30caa61ae76ca0a.rrd':iowait:AVERAGE";
+        $out = "DEF:def_average_iowait=2d/07/2d07f300dbec4dc5a30caa61ae76ca0a.rrd:iowait:AVERAGE";
         $this->assertEquals($out, (string) Assignment::parse($in));
 
         $in = "DEF:'defa'='2d/07/2d07f300dbec4dc5a30caa61ae76ca0a.rrd':'iowait':AVERAGE";
-        $out = "DEF:defa='2d/07/2d07f300dbec4dc5a30caa61ae76ca0a.rrd':iowait:AVERAGE";
+        $out = "DEF:defa=2d/07/2d07f300dbec4dc5a30caa61ae76ca0a.rrd:iowait:AVERAGE";
         $this->assertEquals($out, (string) Assignment::parse($in));
     }
 }
