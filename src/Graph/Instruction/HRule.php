0667c2c6b419dc08a63b81927bc585f1ae1a696d
Thomas Gelf <thomas@gelf.net>
2024-07-11 10:32:14 +0200

    HRule: don't extend DefinitionBasedGraphInstruction

--- src/Graph/Instruction/HRule.php
+++ src/Graph/Instruction/HRule.php
@@ -2,6 +2,9 @@
 
 namespace gipfl\RrdGraph\Graph\Instruction;
 
+use gipfl\RrdGraph\Color;
+use gipfl\RrdGraph\DataType\FloatType;
+use gipfl\RrdGraph\DataType\StringType;
 use gipfl\RrdGraph\Render;
 
 /**
@@ -16,19 +19,49 @@ use gipfl\RrdGraph\Render;
  * --------
  * HRULE:value#color[:[legend][:dashes[=on_s[,off_s[,on_s,off_s]...]][:dash-offset=offset]]]
  */
-class HRule extends DefinitionBasedGraphInstruction
+class HRule implements GraphInstructionInterface
 {
     use Dashes;
 
     const TAG = 'HRULE';
+    protected FloatType $value;
+    protected ?Color $color = null;
+    protected ?StringType $legend = null;
+
+    final public function __construct(FloatType $value, ?Color $color = null, ?StringType $legend = null)
+    {
+        $this->value = $value;
+        $this->color = $color;
+        $this->legend = $legend;
+    }
 
     public function __toString(): string
     {
         return self::TAG
             . ':'
-            . $this->definition
+            . $this->value
             . $this->color
             . Render::optionalParameter($this->legend)
             . $this->renderDashProperties();
+    }
+
+    public static function fromParameters(array $parameters): HRule
+    {
+        $first = array_shift($parameters);
+        $parts = explode('#', $first);
+        $value = FloatType::parse(array_shift($parts));
+        if (! empty($parts)) {
+            $color = new Color(array_shift($parts));
+        } else {
+            $color = null;
+        }
+        // TODO: Dashes?
+        if (! empty($parameters) && $parameters[0] instanceof StringType) {
+            $legend = array_shift($parameters);
+        } else {
+            $legend = null;
+        }
+
+        return  new static($value, $color, $legend);
     }
 }
