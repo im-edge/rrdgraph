280cf42d77bacb375e1f2fca4f6b6bb99564debd
Thomas Gelf <thomas@gelf.net>
2024-07-11 10:22:33 +0200

    DefinitionBasedGraphInstruction: provice common...
    ...methods

--- src/Graph/Instruction/DefinitionBasedGraphInstruction.php
+++ src/Graph/Instruction/DefinitionBasedGraphInstruction.php
@@ -7,8 +7,9 @@ use gipfl\RrdGraph\Data\VariableName;
 use gipfl\RrdGraph\DataType\BooleanType;
 use gipfl\RrdGraph\DataType\StringType;
 use InvalidArgumentException;
+use RuntimeException;
 
-abstract class DefinitionBasedGraphInstruction implements GraphInstructionInterface
+abstract class DefinitionBasedGraphInstruction implements GraphInstructionInterface, InstructionWithVariableInterface
 {
     const TAG = 'DEFINITION_WITHOUT_TAG';
     protected VariableName $definition;
@@ -24,17 +25,61 @@ abstract class DefinitionBasedGraphInstruction implements GraphInstructionInterf
         $this->legend = $legend;
     }
 
-    public static function fromParameters(array $parameters)
+    public function getDefinition(): VariableName
+    {
+        return $this->definition;
+    }
+
+    public function getColor(): ?Color
+    {
+        return $this->color;
+    }
+
+    public function setColor(?Color $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function getLegend(): ?StringType
+    {
+        return $this->legend;
+    }
+
+    public function setLegend(?StringType $legend): void
+    {
+        $this->legend = $legend;
+    }
+
+    public function renameVariable(string $oldName, string $newName)
+    {
+        if ($this->definition->getName() === $oldName) {
+            $this->definition->setName($newName);
+        }
+    }
+
+    public static function fromParameters(array $parameters): self
     {
         $variable = array_shift($parameters);
         $parts = explode('#', $variable);
+        if ($parts[0] === '0') {
+            throw new RuntimeException('$parts[0] is "0": ' . var_export($parts, true)); // TODO: when does this happen?
+        }
         $varName = new VariableName(array_shift($parts));
         if (! empty($parts)) {
             $color = new Color(array_shift($parts));
         } else {
             $color = null;
         }
+        if (! empty($parts)) {
+            $color2 = new Color(array_shift($parts));
+        } else {
+            $color2 = null;
+        }
         foreach ($parameters as $key => $parameter) {
+            if (preg_match('/^([^\'][a-zA-Z+])=(.+)$/', $parameter, $match)) {
+                $key = $match[1];
+                $parameter = $match[2];
+            }
             if (isset(static::OPTIONAL_PARAMETERS[$parameter])) {
                 $class = static::OPTIONAL_PARAMETERS[$parameter];
                 $parameters[$key] = $class::parse($parameter);
@@ -49,6 +94,16 @@ abstract class DefinitionBasedGraphInstruction implements GraphInstructionInterf
         }
 
         $self =  new static($varName, $color, $legend);
+        if ($color2) {
+            if (method_exists($self, 'setColor2')) {
+                $self->setColor2($color2);
+            } else {
+                throw new RuntimeException(sprintf(
+                    '%s has no second color, failed to instantiate from parameters: ',
+                    get_class($self)
+                ));
+            }
+        }
         foreach ($parameters as $boolean) {
             if ($boolean instanceof BooleanType) {
                 $self->{$boolean->getLabel()} = $boolean;
